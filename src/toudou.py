49c9295f91c335da9de2aa626babc45d4ba647b3
boucanier <chironjules.78@gmail.com>
2024-02-01 15:08:54 +0100

    Update function

--- src/toudou.py
+++ src/toudou.py
@@ -39,3 +39,41 @@ def read():
         todo = pickle.load(f)
     
     click.echo(todo)
+
+
+
+@cli.command()
+def update():
+    with open('todo.p', 'rb') as f:
+        todo = pickle.load(f)
+        
+    click.echo(todo)
+    
+    cont = True
+
+    while cont :
+        click.echo("What do you want to change ? [task: t / due: d / state: s / nothing: n]")
+        ans = click.getchar()
+
+        if ans in ["t", "d", "s"]:
+            if ans == "t" :
+                todo.task = click.prompt("New task")
+            if ans == "d" :
+                new_date = click.prompt("New date", default=date.today().strftime("%d/%m/%Y")).split("/")
+                todo.due = date(int(new_date[2]), int(new_date[1]), int(new_date[0]))
+            if ans == "s" :
+                todo.complete = not todo.complete
+
+            click.echo()
+            click.echo(todo)
+            click.echo()
+
+
+        click.echo("Continue changes ? [Y/n]")
+        ans = click.getchar()
+
+        if ans == "n" :
+            cont = False
+
+    with open('todo.p', 'wb') as f:
+        pickle.dump(todo, f)
