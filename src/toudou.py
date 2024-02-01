5b1ec993ea08d400587458f9f4317b44ec3f8bea
boucanier <chironjules.78@gmail.com>
2024-02-01 13:55:24 +0100

    Todo creation

--- src/toudou.py
+++ src/toudou.py
@@ -1,5 +1,7 @@
 import click
 import uuid
+from datetime import date
+import pickle
 
 from dataclasses import dataclass
 
@@ -8,6 +10,8 @@ from dataclasses import dataclass
 class Todo:
     id: uuid.UUID
     task: str
+    due: date
+    complete: bool
 
 
 @click.group()
@@ -17,6 +21,9 @@ def cli():
 
 @cli.command()
 @click.option("-t", "--task", prompt="Your task", help="The task to remember.")
-def display(task: str):
-    todo = Todo(uuid.uuid4(), task)
+@click.option("-d", "--due", type=click.DateTime(formats=["%d/%m/%Y"]), default=date.today().strftime("%d/%m/%Y"), prompt="Due", help="Due of task.")
+def create(task: str, due: date):
+    todo = Todo(uuid.uuid4(), task, due, False)
     click.echo(todo)
+    with open('todo.p', 'wb') as f:
+        pickle.dump(todo, f)
