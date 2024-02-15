b41b2b78f2462d133a85607e0cf3d15f33f924fa
boucanier <chironjules.78@gmail.com>
2024-02-15 14:34:25 +0100

    Write one task and read all

--- src/toudou/models.py
+++ src/toudou/models.py
@@ -1,14 +1,25 @@
 import os
 import pickle
+import select
 import uuid
 
 from dataclasses import dataclass
 from datetime import datetime
-from sqlalchemy import create_engine, Column, String, Boolean, DateTime, Table, MetaData, Uuid
+from sqlalchemy import create_engine, Column, String, Boolean, DateTime, Table, MetaData, Uuid, select
 
 
 TODO_FOLDER = "db"
 
+
+metadata = MetaData()
+todos_table = Table(
+        "todos",
+        metadata,
+        Column("id", Uuid, primary_key=True, default=uuid.uuid4),
+        Column("task", String, nullable=False),
+        Column("complete", Boolean, nullable=False),
+        Column("due", DateTime, nullable=True)
+    )
 
 @dataclass
 class Todo:
@@ -24,14 +35,6 @@ def init_db() -> None:
 
     engine = create_engine("sqlite:///" + TODO_FOLDER + "/todos.db", echo=True)
     metadata = MetaData()
-    todos_table = Table(
-        "todos",
-        metadata,
-        Column("id", Uuid, primary_key=True, default=uuid.uuid4),
-        Column("task", String, nullable=False),
-        Column("complete", Boolean, nullable=False),
-        Column("due", DateTime, nullable=True)
-    )
 
     metadata.create_all(engine)
 
@@ -42,8 +45,18 @@ def read_from_file(filename: str) -> Todo:
 
 
 def write_to_file(todo: Todo, filename: str) -> None:
-    with open(os.path.join(TODO_FOLDER, filename), "wb") as f:
-        pickle.dump(todo, f)
+    # Avec Pickle
+    # with open(os.path.join(TODO_FOLDER, filename), "wb") as f:
+    #     pickle.dump(todo, f)
+
+    # Avec SQLAlchemy
+    engine = create_engine("sqlite:///" + TODO_FOLDER + "/todos.db", echo=True)
+    metadata = MetaData()
+
+    stmt = todos_table.insert().values(task=todo.task, complete=todo.complete, due=todo.due)
+
+    with engine.begin() as conn:
+        conn.execute(stmt)
 
 
 def create_todo(
@@ -60,11 +73,29 @@ def get_todo(id: uuid.UUID) -> Todo:
 
 
 def get_all_todos() -> list[Todo]:
+    # Avec Pickle
+    # result = []
+    # for id in os.listdir(TODO_FOLDER):
+    #     todo = get_todo(uuid.UUID(id))
+    #     if todo:
+    #         result.append(todo)
+    # return result
+
+    # Avec SQLAlchemy
+    engine = create_engine("sqlite:///" + TODO_FOLDER + "/todos.db", echo=True)
+    metadata = MetaData()
+
     result = []
-    for id in os.listdir(TODO_FOLDER):
-        todo = get_todo(uuid.UUID(id))
-        if todo:
-            result.append(todo)
+
+    with engine.connect() as conn:
+        for row in conn.execute(select(todos_table)):
+            result.append(Todo(
+                row.id,
+                row.task,
+                row.complete,
+                row.due
+            ))
+
     return result
 
 
