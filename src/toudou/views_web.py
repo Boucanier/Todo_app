a007698ead852e5aea8d053b8bbf9fb5740623fc
boucanier <chironjules.78@gmail.com>
2024-02-29 15:04:18 +0100

    Form to add todo

--- src/toudou/views_web.py
+++ src/toudou/views_web.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import date, datetime
 
 import toudou.models as models
 
 app = Flask(__name__)
 
+@app.route("/toudou/", methods=["GET"])
+def controller():
+    if request.args.get('action') == "add":
+        models.create_todo(request.args.get('task', ''), due=((datetime.strptime(request.args.get('due', ''), "%Y-%m-%d")).date() if request.args.get('due') else None)) # type: ignore
+    return render_template("index.html", todos=models.get_all_todos())
+
 @app.route("/toudou/")
-def index(todos=None):
+def index():
     return render_template("index.html", todos=models.get_all_todos())
