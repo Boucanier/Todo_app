e5da4b65e5381f3456d13596eac4965907bf05ae
boucanier <chironjules.78@gmail.com>
2024-03-06 11:45:46 +0100

    Export csv option

--- src/toudou/services.py
+++ src/toudou/services.py
@@ -7,15 +7,17 @@ from datetime import datetime
 from toudou.models import create_todo, get_all_todos, Todo
 
 
-def export_to_csv() -> io.StringIO:
-    output = io.StringIO()
-    csv_writer = csv.DictWriter(
-        output,
-        fieldnames=[f.name for f in dataclasses.fields(Todo)]
-    )
-    for todo in get_all_todos():
-        csv_writer.writerow(dataclasses.asdict(todo))
-    return output
+def export_to_csv() -> str :
+
+    with open("todos.csv", "w") as file:
+        path = "db/todos.csv"
+        csv_writer = csv.writer(file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([f.name for f in dataclasses.fields(Todo)])
+
+        for todo in get_all_todos():
+            csv_writer.writerow(todo.__dict__.values())
+
+    return path
 
 
 def import_from_csv(csv_file: io.StringIO) -> None:
--- src/toudou/views_web.py
+++ src/toudou/views_web.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from datetime import datetime
 import uuid
+from toudou import services
 
 import toudou.models as models
 
@@ -10,20 +11,26 @@ app = Flask(__name__)
 
 @app.route("/toudou/", methods=["GET"])
 def controller():
-    if request.args.get('action') == "add":
-        models.create_todo(request.args.get('task', ''), due=((datetime.strptime(request.args.get('due', ''), "%Y-%m-%d")).date() if request.args.get('due') else None)) # type: ignore
+    if request.args.get('id', ''):
+        if request.args.get('action') == "add":
+            models.create_todo(request.args.get('task', ''), due=((datetime.strptime(request.args.get('due', ''), "%Y-%m-%d")).date() if request.args.get('due') else None)) # type: ignore
 
-    elif request.args.get('action') == "update":
-        new_comp = False
-        if request.args.get('complete') :
-            new_comp = True
-        models.update_todo(uuid.UUID(request.args.get('id', '')), request.args.get('task', ''), new_comp, due=((datetime.strptime(request.args.get('due', ''), "%Y-%m-%d")).date() if request.args.get('due') else None)) # type: ignore
-    
-    elif request.args.get('action') == "delete":
-        models.delete_todo(uuid.UUID(request.args.get('id', '')))
+        elif request.args.get('action') == "update":
+            new_comp = False
+            if request.args.get('complete') :
+                new_comp = True
+            models.update_todo(uuid.UUID(request.args.get('id', '')), request.args.get('task', ''), new_comp, due=((datetime.strptime(request.args.get('due', ''), "%Y-%m-%d")).date() if request.args.get('due') else None)) # type: ignore
         
+        elif request.args.get('action') == "delete":
+            models.delete_todo(uuid.UUID(request.args.get('id', '')))
+            
     return render_template("index.html", todos=models.get_all_todos())
 
 @app.route("/toudou/")
 def index():
     return render_template("index.html", todos=models.get_all_todos())
+
+@app.route("/toudou/download/")
+def download():
+    path = "../../" + services.export_to_csv()
+    return send_file(path, as_attachment=True)
