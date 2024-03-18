2caa3111d100cadaf633ea2b63fdbea639943ef5
boucanier <chironjules.78@gmail.com>
2024-03-18 19:02:44 +0100

    INFO logging

--- src/toudou/views.py
+++ src/toudou/views.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, render_template, request, send_file, Blueprint, url_for
 from datetime import datetime
-import uuid, os
+import uuid, os, logging
 from toudou import services, config
 from toudou.forms import AddForm, UpdateForm, DeleteForm
 
@@ -13,6 +13,7 @@ web_ui = Blueprint('web_ui', __name__, url_prefix="/")
 @web_ui.route("/", defaults={"page": "index"})
 @web_ui.route("/<page>")
 def show(page):
+    logging.info(f"Rendering {page}.html")
     return render_template(f"{page}.html", todos=models.get_all_todos(), add_form=AddForm(), update_form=UpdateForm(), delete_form=DeleteForm())
 
 
@@ -27,14 +28,11 @@ def controller():
         - Returns :
             - (str) : the HTML for the index page
     """
-    if request.method == "GET":
-        if request.args.get('action') == "delete":
-            models.delete_todo(uuid.UUID(request.args.get('id', '')))
-
-    if request.method == "POST":            
+    if request.method == "POST":
         if request.form.get('action') == "add":
             form = AddForm()
             if form.validate_on_submit():
+                logging.info(f"Adding a new Todo: {request.form.get('task', '')}")
                 models.create_todo(request.form.get('task', ''), due=((datetime.strptime(request.form.get('due', ''), "%Y-%m-%d")).date() if request.form.get('due') else None)) # type: ignore
             else:
                 return redirect(url_for('web_ui.show'))
@@ -43,9 +41,10 @@ def controller():
             if request.form.get('action') == "update":
                 form = UpdateForm()
                 new_comp = False
-                if request.form.get('complete') :
+                if request.form.get('complete'):
                     new_comp = True
                 if form.validate_on_submit():
+                    logging.info(f"Updating Todo: {request.form.get('task', '')}")
                     models.update_todo(uuid.UUID(request.form.get('id', '')), request.form.get('task', ''), new_comp, due=((datetime.strptime(request.form.get('due', ''), "%Y-%m-%d")).date() if request.form.get('due') else None)) # type: ignore
                 else:
                     return redirect(url_for('web_ui.show'))
@@ -53,6 +52,7 @@ def controller():
             elif request.form.get('action') == "delete":
                 form = DeleteForm()
                 if form.validate_on_submit():
+                    logging.info(f"Deleting Todo: {request.form.get('id', '')}")
                     models.delete_todo(uuid.UUID(request.form.get('id', '')))
                 else:
                     return redirect(url_for('web_ui.show'))
@@ -63,6 +63,7 @@ def controller():
             filename = os.path.join(config['UPLOAD_FOLDER'], file.filename)
             file.save(filename)
             file = open(filename)
+            logging.info(f"Importing Todos from CSV: {filename}")
             services.import_from_csv(file)
             
     return redirect(url_for('web_ui.show'))
@@ -79,6 +80,7 @@ def export():
         - Returns :
             - (file) : the CSV file of all the Todos
     """
+    logging.info(f"Exporting Todos to CSV")
     path = "../../" + services.export_to_csv()
     return send_file(path, as_attachment=True)
 
